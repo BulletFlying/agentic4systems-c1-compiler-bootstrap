2151a7afe00da65a7d683523225f1f40fa8e9f98
Liangyu Zhang <277838856@qq.com>
2026-07-13 12:55:51 +0800

    feat(c1): export local constant folding pass

--- Track-C/C1-compiler/src/aec_c1/passes/__init__.py
+++ Track-C/C1-compiler/src/aec_c1/passes/__init__.py
@@ -3,12 +3,17 @@
 from .base import CompilerPass, PassResult
 from .manager import PassManager, PassRecord
 from .pipelines import build_pipeline
-from .scalar import BasicBlockLocalCSEPass, ConservativeDeadResultEliminationPass
+from .scalar import (
+    BasicBlockLocalCSEPass,
+    ConservativeDeadResultEliminationPass,
+    LocalConstantFoldingPass,
+)
 
 __all__ = [
     "BasicBlockLocalCSEPass",
     "CompilerPass",
     "ConservativeDeadResultEliminationPass",
+    "LocalConstantFoldingPass",
     "PassManager",
     "PassRecord",
     "PassResult",
